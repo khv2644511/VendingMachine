286cc9b26a109ef30e7d3000b00911a21e800e29
khv2644511 <khv2644511@naver.com>
2022-11-02 22:59:28 +0900

    Feat: 선택한콜라리스트에 디스플레이

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const Cool_Drink = document.querySelector(".Cool");
 const Green_Drink = document.querySelector(".Green");
 const Orange_Drink = document.querySelector(".Orange");
 
+const getDrinkUl = document.querySelector(".get_drink");
+
+let drinkPrice; // change_ChangeMoney
+
+let clicklist = []; // addSelectedDrinkList
+
 // const drinkPriceObj = {
 //   original_Drink: 1000,
 //   Violet_Drink: 1000,
@@ -30,11 +36,16 @@ let drinkList = [
 ];
 
 drinkList.forEach((el) =>
-  el.addEventListener("click", () => change_ChangeMoney(el))
+  el.addEventListener("click", () => {
+    change_ChangeMoney(el);
+    //콜라 선택 시 획득 리스트에 넣어주는 함수
+    addSelectedDrinkList(el);
+  })
 );
 
 inputMoneyBtn.addEventListener("click", get_show_InputMoneyValue);
 
+// 입금액을 잔액에 보여주는 함수
 function get_show_InputMoneyValue() {
   let inputVlaue = inputMoney.value;
   console.log(typeof inputVlaue);
@@ -49,8 +60,53 @@ function get_show_InputMoneyValue() {
   }
 }
 
-function change_ChangeMoney(event) {
-  console.log(el);
-  // console.log(event.currentTarget);
-  console.log(showChange.textContent);
+// 음료 선택 시 잔액 차감되는 함수
+function change_ChangeMoney(el) {
+  el.childNodes.forEach((x) => {
+    if (x.className === "price") {
+      drinkPrice = parseInt(x.textContent);
+      let calcChange = showChange.textContent;
+      if (calcChange <= 0) {
+        alert("잔액이 부족합니다.");
+      } else {
+        calcChange -= drinkPrice;
+        showChange.innerText = calcChange;
+      }
+
+      // console.log(showChange.textContent - drinkPrice);
+    }
+  });
 }
+function addSelectedDrinkList(el) {
+  // console.log(el.className.split(" ")[1]);
+  const drinkName = el.className.split(" ")[1];
+  const li = document.createElement("li");
+  li.classList.add("get_drink_list");
+
+  if (clicklist.includes(drinkName)) {
+    console.log("중복된 콜라");
+    let drinkCountbuttonClassName = `.btn_get_drink_count.${drinkName}`;
+    const drinkCountButton = document.querySelector(drinkCountbuttonClassName);
+    let drinkCount = parseInt(drinkCountButton.textContent);
+    drinkCount++;
+    drinkCountButton.innerText = drinkCount;
+    // 버튼 값 바꾸기
+  } else {
+    clicklist.push(drinkName);
+    li.innerHTML = `
+    <figure class="get_drink_fig"></figure>
+      <img
+        src="./img/${drinkName}_Cola.svg"
+        alt="Elephant at sunset"
+        class="get_drink_img"
+      />
+      <figcaption>${drinkName}_Cola</figcaption>
+    </figure>
+    <button class="btn_get_drink_count ${drinkName}">1</button>`;
+    getDrinkUl.appendChild(li);
+  }
+}
+
+// function (){
+
+// }
